952c1e527cc68370bbcc56951a22a81a0cc3088d
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-07-30 15:28:55 -0500

    Stage 1 complete

---
+++ hangman.js
@@ -0,0 +1,7 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
+const hangman = ["H", "A", "N", "G", "M", "A", "N"];
+console.log(hangman.join(" "));
+console.log("The game will be available soon.");

c984fa20e3c77bdc874b9248d3728a278a5edda7
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-07-31 14:39:03 -0500

    stage 6 complete

--- hangman.js
+++ hangman.js
@@ -17,16 +17,27 @@ Input a letter: `);
 let replaceCharacter = (string, index, replacement) => string.substring(0, index) + replacement + string.substring(index + 1);
 
 while (attemptsLeft > 0) {
+    if (letters === wordToGuess) {
+        console.log(`\n${letters}\nYou guessed the word!\nYou survived!`);
+        attemptsLeft = 0;
+        break;
+    }
     let letter = getLetter();
     if (!wordToGuess.includes(letter)) {
         console.log("That letter doesn't appear in the word.");
+        attemptsLeft--;
     } else {
+        if (letters.includes(letter)) {
+            console.log("No improvements.");
+            attemptsLeft--;
+        }
         for (let index = 0; index < wordToGuess.length; index++) {
             if (letter === wordToGuess[index]) {
                 letters = replaceCharacter(letters, index, letter);
             }
         }
     }
-    attemptsLeft--;
 }
-console.log("\nThanks for playing!");
+if (letters !== wordToGuess) {
+    console.log("\nYou lost!");
+}

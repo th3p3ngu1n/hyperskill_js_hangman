a182bb445854e70441b9a79f6029df85e2fdb711
th3p3ngu1n <kristin.b.harris21@gmail.com>
2025-07-31 13:18:48 -0500

    stage 3 complete

--- hangman.js
+++ hangman.js
@@ -3,10 +3,13 @@
 // You will need this in the following stages
 const input = require('sync-input')
 const hangman = ["H", "A", "N", "G", "M", "A", "N"];
-console.log(hangman.join(" "));
-console.log("The game will be available soon.");
-const wordToGuess = "python";
 const survivedMessage = "You survived!";
 const lostMessage = "You lost!"
-guess = input("Guess the word: ")
-console.log(guess === wordToGuess ? survivedMessage : lostMessage);
+const wordsToGuess = ["python", "java", "swift", "javascript"];
+const wordToGuess = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+
+console.log(hangman.join(" "));
+
+let guess = () => input("Guess the word: ");
+let checkGuess = (guess) => guess === wordToGuess ? survivedMessage : lostMessage;
+console.log(checkGuess(guess()));
